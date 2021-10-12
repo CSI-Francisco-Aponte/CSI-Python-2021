36291c4fc94eea9ab85dc4feeb6db2b25eec95c5
CSI-Francisco-Aponte <24-002@sanignacio.pr>
2021-10-12 10:32:47 -0400

    kept working on proyect

---
+++ Projects/Projectile-Motion/ExperimentData.py
@@ -0,0 +1,9 @@
+class experimentData:
+    def __innit__(self,gun: str, cartridge: str, bullet: str, velocity: float, buildingName: str, buildingHeight: float, gravity: float):
+        self.gun = gun
+        self.cartridge = cartridge
+        self.bullet = bullet
+        self.velocity = velocity
+        self.buildingName = buildingHeight
+        self.buildingHeight = buildingHeight
+        self.gravity = gravity
