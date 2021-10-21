e18655b38557ac4a33a5a79c86bc73c2e33b0320
CSI-Francisco-Aponte <24-002@sanignacio.pr>
2021-10-21 10:32:35 -0400

    kept working on project

--- Projects/Projectile-Motion/ExperimentData.py
+++ Projects/Projectile-Motion/ExperimentData.py
@@ -1,5 +1,5 @@
-class experimentData:
-    def __innit__(self,gun: str, cartridge: str, bullet: str, velocity: float, buildingName: str, buildingHeight: float, gravity: float):
+class ExperimentData:
+    def __init__(self,gun: str, cartridge: str, bullet: str, velocity: float, buildingName: str, buildingHeight: float, gravity: float):
         self.gun = gun
         self.cartridge = cartridge
         self.bullet = bullet
--- Projects/Projectile-Motion/Projectile-Motion.py
+++ Projects/Projectile-Motion/Projectile-Motion.py
@@ -1,14 +1,53 @@
 import math
+import os
+from ExperimentData import ExperimentData
+from pathlib import Path
+import json
 
 # Here i defined the variables
-gun = "APB"
-cartridge = "9x18mm"
-bullet = "9x18mm PM BZhT gzh"
-velocity = 325
-buildingName = "Birj Khalifa"
-buildingHeight = 828
+# gun = "APB"
+# cartridge = "9x18mm"
+# bullet = "9x18mm PM BZhT gzh"
+# velocity = 325
+# buildingName = "Birj Khalifa"
+# buildingHeight = 828
 
-time = math.sqrt((2*buildingHeight)/9.8)
-distance = velocity * time
+# Here i told the program to use the variables i just defined , specifically: velocity and building height, to use math to find out the time the bullet wil travel, to then find the distance the bulllet will travel, considering there is no air resistance.
+# time = math.sqrt((2*buildingHeight)/9.8)
+# distance = velocity * time
 
-print(f"In this experiment, we are climbing {buildingHeight} meters to the top of {buildingName} and shooting a {gun} with a cartidge of {cartridge} using a {bullet}. The bullet will travel with a velocity of {velocity}m/s and a distance of {distance}meters ")
+# Here i made a descriptive paragrapgh describing the experiment using the variables and the results from the previous equation.
+# print(f"In this experiment, we are climbing {buildingHeight} meters to the top of {buildingName} and shooting a {gun} with a cartidge of {cartridge} using a {bullet}. Considering there is no air resistance, the bullet will travel with a velocity of {velocity}m/s and a distance of {distance}meters ")
+
+def calculateBulletDT(experimentData: ExperimentData):
+    time = math.sqrt(2*experimentData.buildingHeight / experimentData.gravity)
+    distance = experimentData.velocity * time
+
+# experimentalData = {"gun" : "APB"
+# "cartridge" : "9x18mm"
+# "bullet" : "9x18mm PM BZhT gzh"
+# "velocity" : 325
+# "buildingName" : "Birj Khalifa"
+# "buildingHeight" : 828}
+
+    print(f"In this experiment, we are climbing {experimentData.buildingHeight} meters to the top of {experimentData.buildingName} and shooting a {experimentData.gun} with a cartidge of {experimentData.cartridge} using a {experimentData.bullet}. Considering there is no air resistance, the bullet will travel with a velocity of {experimentData.velocity}m/s and a distance of {distance}meters ")
+
+# calculateBulletDT("APB", "9x18mm", "9x18mm PM BZhT gzh", 325.00, "Birj Khalifa", 828.00)
+
+
+myDataSet = [
+
+ExperimentData("APB", "9x18mm", "9x18mm PM BZhT gzh", 325.00, "Birj Khalifa", 828.00, 9.8),
+ExperimentData("APB", "9x18mm", "9x18mm PM BZhT gzh", 325.00, "Birj Khalifa", 828.00, 9.8),
+ExperimentData("APB", "9x18mm", "9x18mm PM BZhT gzh", 325.00, "Birj Khalifa", 828.00, 9.8),
+ExperimentData("APB", "9x18mm", "9x18mm PM BZhT gzh", 325.00, "Birj Khalifa", 828.00, 9.8),
+ExperimentData("APB", "9x18mm", "9x18mm PM BZhT gzh", 325.00, "Birj Khalifa", 828.00, 9.8)
+]
+
+calculateBulletDT(mydata[0])
+
+myOutputPath = Path(__file__) . parents[0]
+myOutputFilePath = os.path.join(myOutputPath, 'ExperimentData.json')
+
+with open (myOutputFilePath, 'w') as outfile:
+    json.dump(mydata.__dict__, outfile)
