e9b029afb770a1d9cb38ad52fe0e40b5956ab5d9
CSI-Francisco-Aponte <24-002@sanignacio.pr>
2022-01-31 10:24:43 -0400

    deseralization

--- Modules/Module1/Codingbat.py
+++ Modules/Module1/Codingbat.py
@@ -1,4 +1,2 @@
 def extra_end(str):
     return 3 * str[-2:]
-
-extra_end("Quintana")
--- Projects/Web-Servers/RandomApp.py
+++ Projects/Web-Servers/RandomApp.py
@@ -1,4 +1,4 @@
-class RandomApps:
+class RandomApp:
     def __init__(self, id: int, uid: str, app_name: str, app_version: str, app_author: str, app_semantic_version: str, app_major_version: str, app_minor_version: str, app_patch_version: str):
         self.id = id
         self.uid = uid
