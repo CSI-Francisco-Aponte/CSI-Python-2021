83a039749611c7966da97c77fd31f6a2d52aab70
CSI-Francisco-Aponte <24-002@sanignacio.pr>
2022-02-16 10:35:06 -0400

    Update Hangman.py

--- Projects/Hangman/Hangman.py
+++ Projects/Hangman/Hangman.py
@@ -1,5 +1,7 @@
 import random
-pueblos = ["Adjuntas", "Aguada", "Aguadilla", "Aguas Buenas", "Aibonito", "Arecibo", "Arroyo", "Añasco", "Barceloneta",  "Barranquitas",  "Bayamón",  "Cabo Rojo" , "Caguas",  "Camuy",  "Canóvanas",  "Carolina",  "Cataño",  "Cayey",  "Ceiba",  "Ciales",  "Cidra",  "Coamo",  "Comerío",  "Corozal",  "Culebra",  "Dorado",  "Fajardo",  "Florida",  "Guayama",  "Guayanilla",  "Guaynabo",  "Gurabo",  "Guánica",  "Hatillo",  "Hormigueros",  "Humacao",  "Isabela",  "Jayuya",  "Juana Díaz" , "Juncos",  "Lajas",  "Lares",  "Las Marías" , "Las Piedras" , "Loiza",  "Luquillo",  "Manatí",  "Maricao",  "Maunabo",  "Mayagüez",  "Moca",  "Morovis",  "Naguabo",  "Naranjito",  "Orocovis",  "Patillas",  "Peñuelas",  "Ponce",  "Quebradillas",  "Rincón",  "Rio Grande" , "Sabana Grande", "Salinas",  "San Germán" , "San Juan" , "San Lorenzo" "San Sebastián" , "Santa Isabel" , "Toa Alta" , "Toa Baja", "Trujillo Alto" , "Utuado",  "Vega Alta" , "Vega Baja" , "Vieques",  "Villalba",  "Yabucoa",  "Yauco"]
+
+
+
 hang_man = ['''
    +---+
        |
@@ -17,7 +19,7 @@ hang_man = ['''
    |   |
        |
       ===''', '''
-
+12
    +---+
    O   |
   /|   |
@@ -41,3 +43,15 @@ hang_man = ['''
   /|\  |
   / \  |
       ===''']
+
+print(hang_man[3])
+
+def answer(pueblos):
+   pueblos = ["Adjuntas", "Aguada", "Aguadilla", "Aguas Buenas", "Aibonito", "Arecibo", "Arroyo", "Añasco", "Barceloneta",  "Barranquitas",  "Bayamón",  "Cabo Rojo" , "Caguas",  "Camuy",  "Canóvanas",  "Carolina",  "Cataño",  "Cayey",  "Ceiba",  "Ciales",  "Cidra",  "Coamo",  "Comerío",  "Corozal",  "Culebra",  "Dorado",  "Fajardo",  "Florida",  "Guayama",  "Guayanilla",  "Guaynabo",  "Gurabo",  "Guánica",  "Hatillo",  "Hormigueros",  "Humacao",  "Isabela",  "Jayuya",  "Juana Díaz" , "Juncos",  "Lajas",  "Lares",  "Las Marías" , "Las Piedras" , "Loiza",  "Luquillo",  "Manatí",  "Maricao",  "Maunabo",  "Mayagüez",  "Moca",  "Morovis",  "Naguabo",  "Naranjito",  "Orocovis",  "Patillas",  "Peñuelas",  "Ponce",  "Quebradillas",  "Rincón",  "Rio Grande" , "Sabana Grande", "Salinas",  "San Germán" , "San Juan" , "San Lorenzo" "San Sebastián" , "Santa Isabel" , "Toa Alta" , "Toa Baja", "Trujillo Alto" , "Utuado",  "Vega Alta" , "Vega Baja" , "Vieques",  "Villalba",  "Yabucoa",  "Yauco"]
+   return random.choice(pueblos)
+
+def getInput():
+   letter = input("Chose letter")
+
+   If(len(letter)!= 1)
+      print("error")
