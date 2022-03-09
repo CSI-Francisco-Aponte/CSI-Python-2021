ffbba07dcb9d2d3d73a325896984baf570c49af9
CSI-Francisco-Aponte <24-002@sanignacio.pr>
2022-03-09 10:24:56 -0400

    Update Snake.py

--- Projects/Snake/Snake.py
+++ Projects/Snake/Snake.py
@@ -10,6 +10,15 @@ dis=pygame.display.set_mode((400,300))
 #Thiis is used to make any changes to the screen
 pygame.display.update()
 
+# This makes the window that opens, the screen, to have the caption or be named "Snake game by Edureka"
+pygame.display.set_caption('Snake game by Edureka')
+
+# First a variable is created called game over and then a loop is created using that variable. The loop is runs while the game is not over. This causes the game not to open and close immedietly as it previously did.
+game_over=False
+while not game_over:
+    for event in pygame.event.get():
+        print(event)
+
 #This ends the game
 pygame.quit()
 quit()
