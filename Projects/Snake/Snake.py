a8d4aa636c801a9cf7e2123c4a90c518b0f83dc6
CSI-Francisco-Aponte <24-002@sanignacio.pr>
2022-03-09 10:16:43 -0400

    Create Snake.py

---
+++ Projects/Snake/Snake.py
@@ -0,0 +1,15 @@
+#the screen of the game is created using pygame, therefore the program needs pygame
+import pygame
+
+#This begins the game
+pygame.init()
+
+#This creates the screen display of the game and sets its dimensions
+dis=pygame.display.set_mode((400,300))
+
+#Thiis is used to make any changes to the screen
+pygame.display.update()
+
+#This ends the game
+pygame.quit()
+quit()
