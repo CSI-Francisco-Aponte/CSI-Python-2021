e44152af3bfae98387e18a6346e583cd352860bf
CSI-Francisco-Aponte <24-002@sanignacio.pr>
2022-04-03 21:37:08 -0400

    Corrected 5th block of code for snake.py

--- Projects/Snake/Snake.py
+++ Projects/Snake/Snake.py
@@ -16,7 +16,7 @@ dis=pygame.display.set_mode((800,600))
 pygame.display.set_caption('Snake game by Edureka')
 
 # First a variable is created called game over and then a loop is created using that variable. The loop is runs while the game is not over. This causes the game not to open and close immedietly as it previously did.
-game_over=False
+game_over = False
 
 #Here 2 variables are created x1 and y1 which are manipulated later in the code to change the x and y values of the snake
 x1 = 300
@@ -29,21 +29,28 @@ y1_change = 0
 #This creates a timer in the game
 clock = pygame.time.Clock()
  
-#This is the code used to control the snake, every time the arrows are pressed, the snake changes x or y by 10 depending on which arrow in which direction is pressed.
+#This is the block of code used to control the snake, every time the arrows are pressed, the snake changes x or y by 10 depending on which arrow in which direction is pressed.
 while not game_over:
+    #This creates a for loop with everything in the list
     for event in pygame.event.get():
+    #Here the program notices if the user wants to close the game, it switches the variable game_over to true and closes the program
         if event.type == pygame.QUIT:
             game_over = True
+        #This uses the function KEYDOWN to move the snake
         if event.type == pygame.KEYDOWN:
+        #Here if the left arrow is pressed, the snake moves -10 spaces in the x axis or 10 spaces to the left
             if event.key == pygame.K_LEFT:
                 x1_change = -10
                 y1_change = 0
+        #Here if the right arrow is pressed, the snake moves 10 spaces in the x axis, or 10 spaces to the right.
             elif event.key == pygame.K_RIGHT:
                 x1_change = 10
                 y1_change = 0
+        #Here if the up arrow is pressed, the snake moves -10 spaces in the y axis, or up ten spaces
             elif event.key == pygame.K_UP:
                 y1_change = -10
                 x1_change = 0
+        #Here if the down arrow is pressed, the snake moves 10 spaces in the y axis, or 10 spaces down
             elif event.key == pygame.K_DOWN:
                 y1_change = 10
                 x1_change = 0
@@ -52,22 +59,16 @@ while not game_over:
 # the variables x1 and y1 change as long as the x1_change and y1_change varibles changes too
     x1 += x1_change
     y1 += y1_change
+#Here it makes the entire display completely white
     dis.fill(white)
 # this draws a rectangle on the display colored black on the corrdinates of x1 and y1 with the dimensions 10x10
     pygame.draw.rect(dis, black, [x1, y1, 10, 10])
 
-#This makes so when the close button on the screen is pressed, the screen quits or close
-while not game_over:
-    for event in pygame.event.get():s
-        if event.type==pygame.QUIT:
-            game_over=True
-#Here the the snake is drawn. To draw the snake the function draw.rect() is used, then the variable made before makes the rectangle blue and after that the numbers represent the dimension adn the coordinates of the rectangle drawn. The first two is the coordinates, the last two are the dimensions.
-    pygame.draw.rect(dis,blue,[200,150,10,10])
 #This piece of coding updates the screen.
     pygame.display.update()
 
 #This sets the speed that the timer runs.
-clock.tick(30)
+    clock.tick(30)
 
 #This ends the game
 pygame.quit()
